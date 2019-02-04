657e9aa14d6d19a998b5f89315bbcd478075d0e1
Slabko Vlad <vladislavslsl18@gmail.com>
2019-02-04 17:03:21 +0300

    App Center Distribute

--- app/src/main/java/by/bsu/trainingtask1/MainActivity.java
+++ app/src/main/java/by/bsu/trainingtask1/MainActivity.java
@@ -3,6 +3,7 @@ import com.crashlytics.android.Crashlytics;
 import com.microsoft.appcenter.AppCenter;
 import com.microsoft.appcenter.analytics.Analytics;
 import com.microsoft.appcenter.crashes.Crashes;
+import com.microsoft.appcenter.distribute.Distribute;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -16,6 +17,6 @@ public class MainActivity extends AppCompatActivity {
         Fabric.with(this, new Crashlytics());
         setContentView(R.layout.activity_main);
         AppCenter.start(getApplication(), "277a296a-2807-40d5-8008-67b21f30a986",
-                Analytics.class, Crashes.class);
+                Analytics.class, Crashes.class, Distribute.class);
     }
 }
