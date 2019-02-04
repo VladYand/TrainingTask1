04e0e63aa909eb49fc9bd97e9d9b689c1e0d5039
Slabko Vlad <vladislavslsl18@gmail.com>
2019-02-04 14:35:44 +0300

    change min sdk version

--- app/src/main/java/by/bsu/trainingtask1/MainActivity.java
+++ app/src/main/java/by/bsu/trainingtask1/MainActivity.java
@@ -1,4 +1,7 @@
 package by.bsu.trainingtask1;
+import com.microsoft.appcenter.AppCenter;
+import com.microsoft.appcenter.analytics.Analytics;
+import com.microsoft.appcenter.crashes.Crashes;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -9,5 +12,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        AppCenter.start(getApplication(), "277a296a-2807-40d5-8008-67b21f30a986",
+                Analytics.class, Crashes.class);
     }
 }
